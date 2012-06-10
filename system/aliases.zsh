8f250d918b459b941f1359a27a5f641c494d978d
Brad Taylor <brad@getcoded.net>
2012-06-10 16:37:02 +0000

    Allow color ls on linux also

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,4 +7,9 @@ then
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
+else
+  alias ls="ls --color=auto"
+  alias l="ls -lAh --color=auto"
+  alias ll="ls -l --color=auto"
+  alias la='ls -A --color=auto'
 fi
