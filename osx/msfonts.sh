f56a20a487fa9a70f45c6283c373133bbb16375f
Brad Taylor <btaylor@litl.com>
2012-07-25 16:22:21 -0400

    Add msfonts script to download Consolas

---
+++ osx/msfonts.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Downloads and installs MS fonts like Cambria, Consolas, etc
+
+brew install cabextract
+
+TMPDIR=`mktemp -d /tmp/msfonts.XXXXXX`
+cd ${TMPDIR}
+
+curl -O http://download.microsoft.com/download/f/5/a/f5a3df76-d856-4a61-a6bd-722f52a5be26/PowerPointViewer.exe
+cabextract PowerPointViewer.exe
+cabextract ppviewer.cab
+
+cp *.TTF ${HOME}/Library/Fonts/
+
+rm -rf ${TMPDIR}
+brew uninstall cabextract
