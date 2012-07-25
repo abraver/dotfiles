62442be3399325508b113991ad66812a66ce5b83
Brad Taylor <btaylor@litl.com>
2012-07-25 17:08:09 -0400

    Add script to set avatar and default shell

---
+++ osx/user.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Set user picture
+AVATAR_PATH='${HOME}/Pictures/.avatar.png'
+
+cp avatar.png ${AVATAR_PATH}
+dscl . delete /Users/brad Picture 
+dscl . create /Users/brad Picture ${AVATAR_PATH}
+
+# Set zsh as default shell
+chsh /bin/zsh brad
