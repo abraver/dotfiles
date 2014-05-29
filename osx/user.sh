ad18bf31e83b3bdfb48ab07453d7286cafc7808d
Brad Taylor <brad@whistle.com>
2014-05-29 11:02:42 -0700

    Add gradle.properties file

--- osx/user.sh
+++ osx/user.sh
@@ -7,5 +7,7 @@ cp avatar.png ${AVATAR_PATH}
 dscl . delete /Users/brad Picture 
 dscl . create /Users/brad Picture ${AVATAR_PATH}
 
+cp ../gradle/gradle.properties ~/.gradle/
+
 # Set zsh as default shell
-chsh /bin/zsh brad
+chsh -s /bin/zsh brad
