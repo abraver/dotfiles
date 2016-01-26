551df899b30ce426b3818b403d04bf4432b7f840
Brad Taylor <brad@whistle.com>
2016-01-26 11:11:23 -0800

    Update homebrew/cask/mas defaults

--- osx/homebrew.sh
+++ osx/homebrew.sh
@@ -5,10 +5,100 @@ if [ ! -d /usr/local/Cellar ]; then
     brew doctor
 fi
 
-brew install \
-    grc \
-    coreutils \
-    dos2unix \
-    wget \
-    rbenv \
-    pstree
+brew_install_or_upgrade() {
+  if brew_is_installed "$1"; then
+    if brew_is_upgradable "$1"; then
+      brew upgrade "$@"
+    fi
+  else
+    brew install "$@"
+  fi
+}
+
+brew_is_installed() {
+  local name
+  name="$(brew_expand_alias "$1")"
+
+  brew list -1 | grep -Fqx "$name"
+}
+
+brew_is_upgradable() {
+  local name
+  name="$(brew_expand_alias "$1")"
+
+  ! brew outdated --quiet "$name" >/dev/null
+}
+
+brew_expand_alias() {
+  brew info "$1" 2>/dev/null | head -1 | awk '{gsub(/.*\//, ""); gsub(/:/, ""); print $1}'
+}
+
+cask_install_or_upgrade() {
+}
+
+# Core tools
+brew_install_or_upgrade 'grc'
+brew_install_or_upgrade 'coreutils'
+brew_install_or_upgrade 'dos2unix'
+brew_install_or_upgrade 'wget'
+brew_install_or_upgrade 'pstree'
+brew_install_or_upgrade 'docker'
+brew_install_or_upgrade 'dlite'
+brew_install_or_upgrade 'netcat'
+brew_install_or_upgrade 'argon/mas/mas'
+
+# Github tools
+brew_install_or_upgrade 'hub'
+
+# Imaging tools
+brew_install_or_upgrade 'pngcrush'
+
+# Python tools
+brew_install_or_upgrade 'python'
+brew_install_or_upgrade 'numpy'
+
+# Ruby tools
+brew_install_or_upgrade 'rbenv'
+brew_install_or_upgrade 'ruby-build'
+brew_install_or_upgrade 'phantomjs'
+
+# Java tools
+brew_install_or_upgrade 'gradle'
+brew_install_or_upgrade 'maven'
+
+# Node tools
+brew_install_or_upgrade 'node'
+
+# Heroku tools
+brew_install_or_upgrade 'heroku-toolbelt'
+
+# Electron windows installer builder tools
+brew_install_or_upgrade 'wine'
+brew_install_or_upgrade 'mono'
+brew_install_or_upgrade 'makensis'
+
+# Databases
+brew_install_or_upgrade 'postgres'
+brew_install_or_upgrade 'postgis'
+brew_install_or_upgrade 'redis'
+
+# Casks
+brew cask install --appdir="/Applications" 'spotify'
+brew cask install --appdir="/Applications" 'vlc'
+brew cask install --appdir="/Applications" 'java'
+brew cask install --appdir="/Applications" 'dropbox'
+brew cask install --appdir="/Applications" 'firefox'
+
+# Mac App Store apps
+mas install 406056744 # Evernote
+mas install 425424353 # The Unarchiver
+mas install 635758264 # Calca
+mas install 497799835 # Xcode
+mas install 880001334 # Reeder
+mas install 435003921 # Fantastical
+mas install 412056820 # Trillian
+mas install 803453959 # Slack
+mas install 420212497 # Byword
+mas install 504544917 # Clear
+mas install 448003584 # Simplify
+mas install 458034879 # Dash
